ec7a64d2514ebf19a612b12830bf4c2d1058a0cb
IrynaKyslytsia <i.kislica89@gmail.com>
2023-05-06 23:01:11 +0300

    Add Filter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,12 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
 
 class App extends Component {
   state = {
     contacts: [],
-    
+    filter: ''
   };
 
   addContact = (data) => {
@@ -21,13 +22,18 @@ class App extends Component {
     });
   };
 
+  handleChangeFilter = (e) => {
+    this.setState({filter: e.target.value})
+  };
+
   render() {
+    const { filter } = this.state;
     return (
       <div className='container'>        
           <h2 className='sectionTitle'>Phonebook</h2>         
-          <ContactForm onAddContact={this.addContact} />
-        
+          <ContactForm onAddContact={this.addContact} />        
           <h2 className='sectionTitle'>Contacts</h2>
+          <Filter value={filter} onChange={this.handleChangeFilter}/>
           <ContactList contacts={this.state.contacts}  />
         
       </div>
